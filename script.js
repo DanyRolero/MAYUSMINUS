2dcdef6956ea0de6b8ae19d595d5198f54f5e195
Daniel Martinez <dany_rolero@hotmail.com>
2024-09-23 19:00:04 +0200

    Basic Functionalities

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-//-----------------------------------------------------------
+//---------------------------------------------------------------------------------
 // VARIABLES
-//-----------------------------------------------------------
+//---------------------------------------------------------------------------------
 const chars = [
     'a','b','c','d','e',
     'f','g','h','i','j',
@@ -12,33 +12,43 @@ const chars = [
 
 Object.freeze(chars);
 
-let remainingsChars = chars.slice(0,chars.length);
+let remainingsChars;
 let currentQuestionChar = '?';
 
 let currentAnswerChars = [];
 
 let failedChars = [];
 
-let currentLevel = 1;
+let currentLevel = 2;
+let upperCase = true;
 
 let currentQuestionCharHTML = document.getElementById('current-char-question');
 let currentAnswerCharsHTML = document.getElementById('current-chars-answers');
+let resultMessageHTML = document.getElementById('result-message');
 
-//-----------------------------------------------------------
-// FUNCTIONS
-//-----------------------------------------------------------
+//---------------------------------------------------------------------------------
+// CHARS FUNCTIONS
+//---------------------------------------------------------------------------------
 
-//-----------------------------------------------------------
+//---------------------------------------------------------------------------------
+// Establecer alfabeto inicial como letras restantes
+function resetRemainingChars() {
+    remainingsChars = chars.slice(0,chars.length);
+    failedChars = [];
+}
+
+
+//---------------------------------------------------------------------------------
 // Sacar una letra del abecedario aleatoriamente de entre las que se encuentran disponibles
 function updateCurrentQuestionChar() {
     if(remainingsChars.length == 0) throw new Error("No more remaining chars");
     
     let randomIndex = Math.floor(Math.random() * remainingsChars.length);
-    currentQuestionChar = remainingsChars.splice(randomIndex, 1);
+    currentQuestionChar = remainingsChars.splice(randomIndex, 1)[0];
     currentAnswerChars.push(currentQuestionChar);
 }
 
-//-----------------------------------------------------------
+//---------------------------------------------------------------------------------
 // Obtener letras de opciones aleatoriamente
 function addRandomAnswerChar() {
     if(currentAnswerChars.length >= chars.length) throw new Error("No more characters in the alphabet");
@@ -50,7 +60,12 @@ function addRandomAnswerChar() {
     currentAnswerChars.push(randomAnswerChar);
 }
 
-//-----------------------------------------------------------
+//---------------------------------------------------------------------------------
+function addRandomAnswerCharsFromLevel(level) {
+    for(let i = 0; i < level; i++) addRandomAnswerChar();
+}
+
+//---------------------------------------------------------------------------------
 // Revolver orden de posibles respuestas
 function shuffleOrderAnswerChars() {
     let shuffleredAnswers = [];
@@ -62,44 +77,83 @@ function shuffleOrderAnswerChars() {
 }
 
 
-//-----------------------------------------------------------
-// SHOWER FUNCTIONS
-//-----------------------------------------------------------
+//---------------------------------------------------------------------------------
+// HTML FUNCTIONS
+//---------------------------------------------------------------------------------
 function updateCurrentQuestionCharHTML() {
+    if(upperCase) {
+        currentQuestionCharHTML.innerHTML = currentQuestionChar.toLocaleUpperCase();
+        return;
+    }
     currentQuestionCharHTML.innerHTML = currentQuestionChar;
 }
 
-function updateAnswerCarsHTML() {
+//---------------------------------------------------------------------------------
+function updateAnswerCharsHTML() {
     currentAnswerCharsHTML.innerHTML = '';
     for(let i = 0; i < currentAnswerChars.length; i++) {
         let newHTMLbutton = document.createElement('button');
-        newHTMLbutton.textContent = currentAnswerChars[i];
+        newHTMLbutton.textContent = upperCase ? currentAnswerChars[i] : currentAnswerChars[i].toLocaleUpperCase();
+        newHTMLbutton.addEventListener('click', selectChar.bind(newHTMLbutton));
         currentAnswerCharsHTML.appendChild(newHTMLbutton);
     }
 }
 
+//---------------------------------------------------------------------------------
+// GAMEPLAY FUNCTIONS
+//---------------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------------
+// Reaccionar al evento de pulsar en la letra
+function selectChar() {
+    if(this.textContent.toLowerCase() == currentQuestionChar.toLocaleLowerCase()) {
+        win();
+        return;
+    }
+    loose();
+    failedChars.push()
+    this.disabled = true;
+}
+
+//---------------------------------------------------------------------------------
+//Lo que sucede al pulsar el botón de la letra correcta
+function win() {
+    resultMessageHTML.textContent = 'MUY BIEN';
+
+    setTimeout(() => {
+        resultMessageHTML.textContent = '';
+        nextChar();
+    }, 1000);
+}
+
+function loose() {
+    resultMessageHTML.textContent = 'OTRA VEZ';
+}
+
+function nextChar() {
+    currentAnswerChars = [];
+    updateCurrentQuestionChar();
+    addRandomAnswerCharsFromLevel(currentLevel);
+    shuffleOrderAnswerChars();
+    updateCurrentQuestionCharHTML();
+    updateAnswerCharsHTML();
+}
+
+function restart() {
+    resetRemainingChars();
+    updateCurrentQuestionChar();
+    addRandomAnswerCharsFromLevel(currentLevel);
+    shuffleOrderAnswerChars();
+    updateCurrentQuestionCharHTML();
+    updateAnswerCharsHTML();
+}
+
+function repeatOnlyFailChars() {
+
+}
 
 
-
-
-// Añadir letra obtenida a la lista de letras aparecidas
-
-// Cambiar mayúsculas por minúsculas
-
-// Cambiar Familia de fuentes
-
-// Cambiar nivel de dificultad
-
-// Cambiar tema
-
-// Activar desactivar sonido
-
-// Opción Correcta
-
-// Opción Incorrecta
-
-// Letras que hay que repasar (opcional) al recorrer todo el abecedario
-
-// Repasar solo las letras necesarias (opcional)
-
-// Repetir de nuevo
+//---------------------------------------------------------------------------------
+// PROGRAM
+//---------------------------------------------------------------------------------
+restart();
