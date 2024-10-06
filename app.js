f336434a1ef3a3c53efca49ac3ee58b9ac0fec8c
Daniel Martinez <dany_rolero@hotmail.com>
2024-10-06 12:16:48 +0200

    Refactoring

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
+let lastY = 0;
+        let preventPullToRefresh = false;
+
+        // Evento touchstart: Capturamos la posición inicial
+        window.addEventListener('touchstart', (e) => {
+            if (e.touches.length !== 1) return;
+            lastY = e.touches[0].clientY;
+            preventPullToRefresh = window.scrollY === 0;
+        });
+
+        // Evento touchmove: Prevenimos el comportamiento si se desplaza hacia abajo en la parte superior
+        window.addEventListener('touchmove', (e) => {
+            const currentY = e.touches[0].clientY;
+            const upwardScroll = lastY - currentY < 0;
+            if (preventPullToRefresh && upwardScroll) {
+                e.preventDefault();
+            }
+        }, { passive: false });
+
+
 let app = new GamePlayController();
