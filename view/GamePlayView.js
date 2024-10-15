7f006e0e5da27f625a72aeec34b8b93c42fe1415
Daniel Martinez <dany_rolero@hotmail.com>
2024-10-15 19:31:00 +0200

    sound effects

--- view/GamePlayView.js
+++ view/GamePlayView.js
@@ -20,8 +20,8 @@ class GamePlayView extends BaseView {
     addAnswerChoice(content) {
         let answerCharButton = document.createElement('button');
         answerCharButton.textContent = content;
-        answerCharButton.addEventListener('touchstart', () => this.handleButtonClick(answerCharButton), {passive: false});
-        answerCharButton.addEventListener('click', () => this.handleButtonClick(answerCharButton));
+        answerCharButton.addEventListener('mousedown', () => this.handleButtonClick(answerCharButton), {passive: false});
+        //answerCharButton.addEventListener('click', () => this.handleButtonClick(answerCharButton));
 
         this.#answerChoices.appendChild(answerCharButton);       
     }
