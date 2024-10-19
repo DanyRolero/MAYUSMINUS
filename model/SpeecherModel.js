c5d126d02dbca4537ca4e4d87ee8cd13b6a37df8
Danito <dany_rolero@hotmail.com>
2024-10-19 19:28:35 +0200

    AudioModel

--- model/SpeecherModel.js
+++ model/SpeecherModel.js
@@ -1,15 +1,8 @@
 class SpeecherModel {
-  
+
   //---------------------------------------------------------------------------------
   speak(text) {
     var utterance = new SpeechSynthesisUtterance(text);
-      utterance.onstart = function(event) {
-        console.log('Síntesis de voz iniciada');
-      };
-      utterance.onend = function(event) {
-        console.log('Síntesis de voz finalizada');
-      };
-      speechSynthesis.speak(utterance);
-      console.log('Síntesis de voz ejecutada');
+    speechSynthesis.speak(utterance);
   }
 }
