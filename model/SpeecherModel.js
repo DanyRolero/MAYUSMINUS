b7a90a860b494f62e25e73f96681ebc8b1d36029
Danito <dany_rolero@hotmail.com>
2024-10-19 20:03:10 +0200

    AudioModel

--- model/SpeecherModel.js
+++ model/SpeecherModel.js
@@ -3,6 +3,7 @@ class SpeecherModel {
   //---------------------------------------------------------------------------------
   speak(text) {
     var utterance = new SpeechSynthesisUtterance(text);
+    utterance.lang = 'es-ES';
     speechSynthesis.speak(utterance);
   }
 }
