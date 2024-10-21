dcf2f0fe3704d69518e73403f2b915e03d077e00
Daniel Martinez <dany_rolero@hotmail.com>
2024-10-21 15:04:25 +0200

    sound effects

--- app.js
+++ app.js
@@ -7,20 +7,4 @@ document.addEventListener('touchmove', function(event) { event.preventDefault();
 document.addEventListener('gesturestart', function(event) { event.preventDefault(); }, false);
 
 //---------------------------------------------------------------------------------
-// Prevenir zoom al tocar dos veces seguidas en un elemento
-document.addEventListener("touchend", function (event) {
-    let lastTouchEnd = 0;
-    let now = new Date().getTime();
-    if (now - lastTouchEnd <= 300) {
-      event.preventDefault();
-    }
-    lastTouchEnd = now;
-  },
-  false
-);
-
-
-
-
-//---------------------------------------------------------------------------------
 let app = new GamePlayController(); 
