a34ba2570946b30359dbebc8f119af252228e179
Daniel Martinez <dany_rolero@hotmail.com>
2024-10-21 19:13:57 +0200

    sound effects

--- view/StartView.js
+++ view/StartView.js
@@ -9,23 +9,8 @@ class StartView extends BaseView {
         this.#startButtonElement = document.getElementById('start-button');
 
         this.#startButtonElement.addEventListener("click", (event) => {
-            event.preventDefault();
             this.handlerClickButton(); 
         });
-
-        this.#startButtonElement.addEventListener('touchstart', (event) => {
-            event.preventDefault();
-        }, 
-        {passive: false});
-
-
-        this.#startButtonElement.addEventListener("mouseup", (event) => {
-            event.preventDefault();
-            this.handlerClickButton(); 
-        });
-
-
-
     }
 
     bindStartClickButton(handler) {
