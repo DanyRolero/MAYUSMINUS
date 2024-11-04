184febfc909beda04240ef3b2527576dd870b027
Danito <dany_rolero@hotmail.com>
2024-11-04 23:27:23 +0100

    DomElementWrapper class

--- componentBase.js
+++ componentBase.js
@@ -1,5 +1,27 @@
-class ComponentBase {
-    constructor(domElement = null) {
-        if(domElement == null) throw('The component require ');
+class DomElementWrapper {
+    constructor(domElement) {
+        this.domElement = domElement;
+        this.domElementParent = null;
+    }
+
+    //---------------------------------------------------------------------------------
+    show() {
+        this.domElement.classList.remove('hidden');
+    }
+
+    //---------------------------------------------------------------------------------
+    hide() {
+        this.domElement.classList.add('hidden');
+    }
+
+    //---------------------------------------------------------------------------------
+    add(domElementParent) {
+        this.domElementParent = domElementParent;
+        domElementParent.appendChild(this.domElement);
+    }
+
+    //---------------------------------------------------------------------------------
+    remove() {
+        this.domElementParent.removeChild(this.domElement);
     }
 }
