b0078fef9bc1604af294ef2aec66e84ccb95cbec
Danito <dany_rolero@hotmail.com>
2024-11-06 16:58:30 +0100

    SVGcomponent

--- CardCharViewComponent.js
+++ CardCharViewComponent.js
@@ -1,3 +1,36 @@
 class CardCharViewComponent extends ViewComponentBase {
 
+        //---------------------------------------------------------------------------------
+        constructor(svgSpriteData) {
+            super(document.createElement('div'));
+            this.addClass('card-char-svg-comp');
+
+            this.cardSVvgContainer = document.createElement('div');
+
+            this.svgElement = document.createElement('svg');
+            this.svgElement.setAttribute('viewBox', svgSpriteData.viewBox);
+
+            this.useSvgElement = document.createElement('use');
+            this.useSvgElement.setAttribute('href', svgSpriteData.href);
+
+            this.domElement.appendChild(this.cardSVvgContainer);
+            this.cardSVvgContainer.appendChild(this.svgElement);
+            this.svgElement.appendChild(this.useSvgElement);    
+        }
+
+        //---------------------------------------------------------------------------------
+        setBackgroundColor(CSScolorValue) {
+            this.domElement.style.backgroundColor = CSScolorValue;
+        }
+
+        //---------------------------------------------------------------------------------
+        setFillColor(CSScolorValue) {
+            this.svgElement.style.fill = CSScolorValue;
+        }
+
+        //---------------------------------------------------------------------------------
+        setSvgSpriteData(svgSpriteData) {
+            this.svgElement.setAttribute('viewBox', svgSpriteData.viewBox);
+            this.useSvgElement.setAttribute('href', svgSpriteData.href);
+        }
 }
